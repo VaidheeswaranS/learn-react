3157dba6198c5db4b74c78f93488fdeaf9415357
Vaidheeswaran Srinivasan <vsrinivasan@presidio.com>
2025-09-12 15:34:24 +0530

    feat: added the login and logout button change feature

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -1,10 +1,16 @@
-﻿import { APP_LOGO } from "../utils/constants";
+﻿import { useState } from "react";
+import { APP_LOGO } from "../utils/constants";
 
 const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
+
   return (
     <div className="header">
       <div className="logo-container">
         <img className="app-logo" src={APP_LOGO} />
+      </div>
+      <div className="middle-text-container">
+        Hey there! Taste your delicious food !!
       </div>
       <div className="nav-items-container">
         <ul>
@@ -12,10 +18,18 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="login-logout-button"
+            onClick={() => {
+              btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+            }}
+          >
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
   );
-}
+};
 
-export default Header ;
+export default Header;
