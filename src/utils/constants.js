ba556367c8be2f52d1c5cc947e4b3f3f11ff7dc8
Vaidheeswaran Srinivasan <vsrinivasan@presidio.com>
2025-09-15 12:44:48 +0530

    feat: added the restaurant menu feature

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@
 
 /**
  * This might change in future. Go to swiggy.com to get the latest API endpoint
- * Check for the title 'Top restaurant chains in Chennai' in the API response
+ * Check for the title 'Top restaurant chains in Chennai' in the API response from the network tab
  */
 export const RESTAURANT_LIST_API = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0066625&lng=80.2206369&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
 
@@ -10,6 +10,10 @@ export const RESTAURANT_IMAGE = 'https://media-assets.swiggy.com/swiggy/image/up
 
 export const RESTAURANT_MENU_IMAGE = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/';
 
+/**
+ * This might change in future. Go to swiggy.com to get the latest API endpoint
+ * Go inside the restaurants under the title 'Top restaurant chains in Chennai' and then check for API response in network tab
+ */
 export const RESTAURANT_MENU_API = 'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.0066625&lng=80.2206369&restaurantId=';
 
 export const SEARCH_ICON = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp-GnQ-qnGAG90kmtY6R_n_uo3YU8eUnmF4Q&s';
