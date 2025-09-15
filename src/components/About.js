6ff94ef72b52a41d6ff059b28212918edeac7259
Vaidheeswaran Srinivasan <vsrinivasan@presidio.com>
2025-09-15 22:19:50 +0530

    feat: added some code to learn about class based components

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,12 @@
-﻿const About = () => {
+﻿import User from "../components/User"
+import UserClass from "./UserClass";
+
+const About = () => {
   return (
     <div className="about-us">
       <h1>This is About us Page</h1>
+      <User name={"Vaidheeswaran Srinivasan - function"} location={"Chennai - function"} />
+      <UserClass name={"Vaidheeswaran Srinivasan - class"} location={"Chennai - class"} />
     </div>
   );
 }
---
+++ src/components/User.js
@@ -0,0 +1,19 @@
+﻿import { useState } from "react";
+
+const User = (props) => {
+  const [count, setCount] = useState(0);
+  const { name, location } = props ;
+  return (
+    <div className="user-card">
+      <h2>Count: {count}</h2>
+      <button onClick={() => {
+        setCount(count + 1);
+      }}>Increase Count</button>
+      <h2>Name: {name}</h2>
+      <h3>Location: {location}</h3>
+      <h4>Contact: @VaidheeswaranS</h4>
+    </div>
+  );
+}
+
+export default User ;
