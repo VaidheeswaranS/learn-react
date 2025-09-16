88bebc274847351efcfcabd707ca421324a580ca
Vaidheeswaran Srinivasan <vsrinivasan@presidio.com>
2025-09-16 22:36:22 +0530

    feat: added tailwind css styles for header

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -9,33 +9,33 @@ const Header = () => {
   const onlineStatus = useOnlineStatus();
 
   return (
-    <div className="header">
-      <div className="logo-container">
-        <img className="app-logo" src={APP_LOGO} />
-        <div className="app-name">LocateEats</div>
+    <div className="flex justify-between items-center border-b-2 border-b-cyan-700 bg-emerald-100">
+      <div className="flex items-center ml-2">
+        <img className="w-9" src={APP_LOGO} />
+        <div className="font-bold text-rose-800 text-2xl ml-4">LocateEats</div>
       </div>
-      <div className="middle-text-container">
+      <div className="text-lg ml-36">
         Hey there! Taste your delicious food.
       </div>
-      <div className="nav-items-container">
-        <ul>
-          <li>
+      <div className="mr-2">
+        <ul className="flex items-center">
+          <li className="p-2.5 text-lg">
             Online : {onlineStatus ? "✅" : "🔴"}
           </li>
-          <li>
+          <li className="p-2.5 text-lg">
             <Link to={"/"}>Home</Link>
             </li>
-          <li>
+          <li className="p-2.5 text-lg">
             <Link to={"/about"}>About Us</Link>
           </li>
-          <li>
+          <li className="p-2.5 text-lg">
             <Link to={"/contact"}>Contact Us</Link>
           </li>
-          <li>
+          <li className="p-2.5 text-lg">
             <Link>Cart</Link>
           </li>
           <button
-            className="login-logout-button"
+            className="p-2.5 text-lg"
             onClick={() => {
               btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
             }}
